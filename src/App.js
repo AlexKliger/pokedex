73035bf0380f12da16ffd3a375f210e99c541a4e
Alex <alex.kliger@gmail.com>
2022-08-15 16:36:47 -0400

    moved entries state down a level from App component to Screen component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import Header from  './components/Header'
 import Screen from './components/Screen'
 import Interface from './components/interface/Interface'
-import useFetch from "./hooks/useFetch"
-
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon'
 
 function App() {
-  const [url] = useState(BASE_URL + '/?offset=0&limit=151')
-  const [config] = useState({})
-  const {data, loading, error} = useFetch(url, config)
-
   return (
     <div className="pokedex">
       <Header />
-      <Screen entries={data ? data.results : []} />
+      <Screen />
       <Interface />
     </div>
   )
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,10 +1,14 @@
 import React, { useCallback, useState } from "react";
 import Entries from "./Entries";
 import Pokemon from "./pokemon/Pokemon"
+import useFetch from "../hooks/useFetch"
 
-const Screen = ({ entries }) => {
+const Screen = () => {
+    const [url] = useState('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
+    const [config] = useState({})
+    const {data, loading, error} = useFetch(url, config)
+
     const [selectedEntry, setSelectedEntry] = useState('X')
-
     const entrySelected = useCallback((index) => {
         setSelectedEntry(index)
     })
@@ -15,7 +19,7 @@ const Screen = ({ entries }) => {
             ?
             <Pokemon entrySelected={entrySelected} pokemonId={selectedEntry} />
             :
-            <Entries entries={entries} entrySelected={entrySelected}/>
+            <Entries entries={data ? data.results : []} entrySelected={entrySelected}/>
             }
         </div>
     )
