dd763a7f4ec656672f49af1dea8da9d116f62302
Alex <alex.kliger@gmail.com>
2022-08-15 16:25:16 -0400

    lowered state of pokemon info down to Pokemon component

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,24 +1,19 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Entries from "./Entries";
 import Pokemon from "./pokemon/Pokemon"
-import useFetch from "../hooks/useFetch";
-
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
 const Screen = ({ entries }) => {
-    const [url, setUrl] = useState('')
-    const [config] = useState({})
-    const {data, loading, error} = useFetch(url, config)
+    const [selectedEntry, setSelectedEntry] = useState('X')
 
-    function entrySelected(index) {
-        setUrl(BASE_URL + index)
-    }
+    const entrySelected = useCallback((index) => {
+        setSelectedEntry(index)
+    })
 
     return (
         <div className="screen">
-            {data
+            {selectedEntry !== 'X'
             ?
-            <Pokemon pokemon={data} entrySelected={entrySelected} />
+            <Pokemon entrySelected={entrySelected} pokemonId={selectedEntry} />
             :
             <Entries entries={entries} entrySelected={entrySelected}/>
             }
--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -2,31 +2,33 @@ import { useState } from 'react'
 import useFetch from '../../hooks/useFetch'
 import './pokemon.css'
 
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon-species/'
-
-const Pokemon = ({ pokemon, entrySelected }) => {
+const Pokemon = ({ entrySelected, pokemonId }) => {
+  const [url1] = useState('https://pokeapi.co/api/v2/pokemon/' + pokemonId || 'X')
+  const [url2] = useState('https://pokeapi.co/api/v2/pokemon-species/' + pokemonId || 'X')
   const [config] = useState({})
-  console.log('Pokemon(', pokemon, ')')
+  const {data: pokemon1, loading: loading1, error: error1} = useFetch(url1, config)
+  const {data: pokemon2, loading: loading2, error: error2} = useFetch(url2, config)  
+
 
   return (
     <div>
-      {pokemon
+      {pokemon1 && pokemon2
       ?
       (<div className="pokemon">
         <div className="pokemon__info flex">
           <div className="pokemon__image flex flex--column">
-            <img src={pokemon.sprites.versions["generation-i"].yellow.front_transparent} alt={pokemon.name} />
-            <p className="bold">No. { pokemon.id }</p>
+            <img src={pokemon1.sprites.versions["generation-i"].yellow.front_transparent} alt={pokemon1.name} />
+            <p className="bold">No. { pokemon1.id }</p>
           </div>
           <div className="pokemon__stats flex flex--column">
-            <p>{ pokemon.name.toUpperCase() }</p>
-            {/* <p>{ pokemon.genera[7].genus.toUpperCase() }</p> */}
-            <p>WT &nbsp;&nbsp;&nbsp; <b className="bold">{ pokemon.weight }</b></p>
-            <p>HT &nbsp;&nbsp;&nbsp; <b className="bold">{ pokemon.height }</b></p>
+            <p>{ pokemon1.name.toUpperCase() }</p>
+            <p>{ pokemon2.genera[7].genus.toUpperCase() }</p>
+            <p>WT &nbsp;&nbsp;&nbsp; <b className="bold">{ pokemon1.weight }</b></p>
+            <p>HT &nbsp;&nbsp;&nbsp; <b className="bold">{ pokemon1.height }</b></p>
           </div>
         </div>
         <div className="pokemon__text">
-          {/* <p>{pokemon.flavor_text_entries[0].flavor_text}</p> */}
+          <p>{pokemon2.flavor_text_entries[0].flavor_text}</p>
           <button onClick={() => entrySelected('X')}>X</button>
         </div>
       </div>)
