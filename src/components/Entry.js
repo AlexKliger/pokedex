5afe6ce29849d50e42e6dd64bc5b6a960329ffab
Alex <alex.kliger@gmail.com>
2022-11-12 14:08:23 -0500

    styling

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -3,7 +3,10 @@ import React from "react"
 const Entry = ({index, entry, entrySelected }) => {
     return (
         <li className="entry">
-            <button className="entry__button font-size--medium" onClick={() => entrySelected(index)}>{index} {entry.name.toUpperCase()}</button>
+            <button
+                className="entry__button font-size--medium" onClick={() => entrySelected(index)}>
+                {index.toString().padStart(3, "0")} {entry.name.toUpperCase()}
+            </button>
         </li>
     )
 }
