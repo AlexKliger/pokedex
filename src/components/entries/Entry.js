60b6411ad05a39f7fd36bf8e9011d87d7e6d1edc
Alex <alex.kliger@gmail.com>
2022-11-19 16:03:52 -0500

    styling: improved entries interface to look more like original

--- src/components/entries/Entry.js
+++ src/components/entries/Entry.js
@@ -3,10 +3,13 @@ import React from "react"
 const Entry = ({index, entry, entrySelected }) => {
     return (
         <li className="entry">
-            <button
-                className="entry__button font-size--medium" onClick={() => entrySelected(index)}>
-                {index.toString().padStart(3, "0")} {entry.name.toUpperCase()}
-            </button>
+            <span>{index.toString().padStart(3, "0")}</span>
+            <span
+                className="entry__button font-size--medium"
+                onClick={() => entrySelected(index)}
+            >
+                    {entry.name.toUpperCase()}
+            </span>
         </li>
     )
 }
